b70d0a7ee39b8a293b57de47a37aa2cf8d867245
Selanda <64166375+Selanda@users.noreply.github.com>
2020-10-20 16:53:12 -0400

    Update anti-ohno.bat
    Reordered steps for more logical progression. Added extra pause at the end to ensure the user read and completed Step 4 before exiting. Fully pathed icacls.exe to avoid issue cited in bug #1 .
    
    Co-Authored-By: Ryoko Asakura <29420880+RyokoAsakura@users.noreply.github.com>

--- anti-ohno.bat
+++ anti-ohno.bat
@@ -1,9 +1,9 @@
 @echo off
 echo -----
-echo Step 1: Delete unneeded folders
+echo Step 1: Delete unneeded folders (WinREAgent and GetCurrent)
 echo -----
-rmdir /S /Q "C:\$WinREAgent"
-rmdir /S /Q "C:\$GetCurrent"
+rmdir /S /Q "C:\$WinREAgent" 2>NUL
+rmdir /S /Q "C:\$GetCurrent" 2>NUL
 
 echo.
 echo.
@@ -24,13 +24,38 @@ echo.
 echo.
 
 echo -----
-echo Step 3: Delete 'Reboot', 'Reboot_AC', and 'Reboot_Battery' registry keys
+echo Step 3: Hold 'Reboot', 'Reboot_AC', and 'Reboot_Battery' hostage
 echo -----
-echo This part will need to be done manually. Please open regedit as adminstrator and delete the following keys:
+mkdir "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot"
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /setowner %username%
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /inheritance:r
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /grant %username%:F
+mkdir "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC"
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC" /setowner %username%
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC" /inheritance:r
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC" /grant %username%:F
+mkdir "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery"
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery" /setowner %username%
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery" /inheritance:r
+C:\Windows\System32\icacls.exe "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery" /grant %username%:F
+
 echo.
-echo - HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UpdateOrchestrator\Reboot
-echo - HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UpdateOrchestrator\Reboot_AC
-echo - HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery
+echo.
+echo.
+
+
+echo -----
+echo Step 4: Delete 'Reboot', 'Reboot_AC', and 'Reboot_Battery' registry keys
+echo -----
+echo This part will need to be done manually. Please open regedit as adminstrator and navigate to:
+echo.
+echo - HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UpdateOrchestrator\
+echo.
+echo. Delete the following three keys (if present):
+echo      Reboot
+echo      Reboot_AC
+echo      Reboot_Battery
+echo.
 echo.
 echo After deleting the above keys, please press any key to continue.
 pause
@@ -39,24 +64,6 @@ echo.
 echo.
 echo.
 
-echo -----
-echo Step 4: Hold 'Reboot', 'Reboot_AC', and 'Reboot_Battery' hostage
-echo -----
-mkdir "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot"
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /setowner %username%
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /inheritance:r
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot" /grant %username%:F
-mkdir "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC"
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC" /setowner %username%
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC" /inheritance:r
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_AC" /grant %username%:F
-mkdir "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery"
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery" /setowner %username%
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery" /inheritance:r
-icacls "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot_Battery" /grant %username%:F
-echo.
-echo.
-echo.
-echo Press any key to exit.
+echo Process complete. Press any key to exit.
 pause >nul
 exit /B
